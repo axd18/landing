ee11a4cc42390b0c067e97392591eb8e90c031d3
Adri <adrifer.media@gmail.com>
2025-06-19 18:37:08 -0300

    se agrego Google Analitycs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter } from 'react-router-dom';
 import AppRouter from '@/routes/AppRouter';
+import ReactGA from "react-ga4";
+import { useEffect } from 'react';
+
+const GA_MEASUREMENT_ID = "G-CML7FX338X";
 
 function App() {
+  // Es una mejor práctica inicializar dentro de un useEffect para asegurar que se ejecute una sola vez en el cliente.
+  useEffect(() => {
+    ReactGA.initialize(GA_MEASUREMENT_ID);
+  }, []);
+
   return (
-    <>
+    <BrowserRouter>
       <AppRouter />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import LandingPage from '@/pages/Landing/LandingPage';
 import HelpPage from '@/pages/Help/HelpPage';
+import usePageTracking from '../hooks/usePageTracking';
 
 const AppRouter = () => {
+  usePageTracking(); // <-- AHORA FUNCIONA CORRECTAMENTE
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/ayuda" element={<HelpPage />} />
-        {/* Otras rutas... */}
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/ayuda" element={<HelpPage />} />
+      {/* Otras rutas... */}
+    </Routes>
   );
 };
 
